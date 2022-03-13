cb190b6e84eeeaae2617aa9e690214528d5be9fb
casder <casderiopus@gmail.com>
2022-03-13 23:59:59 +0300

    feat: rebase validation to js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
-const days = document.querySelector('.days');
-const hours = document.querySelector('.hours');
-const minutes = document.querySelector('.minutes');
-const seconds = document.querySelector('.seconds');
-const timer = document.querySelector('.timer');
+const REGULAR_EMAIL = /^[^@]+@\w+(\.\w+)+\w$/;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const timer = document.querySelector('.timer');
+    const sendForm = document.querySelector('.sendmail_form');
+
+    sendForm.addEventListener('submit', sendMail);
+
+    async function sendMail(evt) {
+        evt.preventDefault();
+
+        const sendInput = sendForm.querySelector('.form_input');
+
+        console.log(REGULAR_EMAIL.test(sendInput.value));
+
+    }
+
+    setTimer(timer);
+});
 
 function getNormalizedRemaining(remaining) {
     const seconds = Math.floor(remaining / 1000) % 60;
@@ -29,8 +43,12 @@ function setTimer(timerNode) {
     const [day, month, year] = timerNode.getAttribute('data-targetDate').split('-');
     const date = new Date(+year, +month - 1, +day)
 
-    const remaining = getTimeRemaining(date);
+    const days = timerNode.querySelector('.days');
+    const hours = timerNode.querySelector('.hours');
+    const minutes = timerNode.querySelector('.minutes');
+    const seconds = timerNode.querySelector('.seconds');
 
+    const remaining = getTimeRemaining(date);
 
     days.textContent = '' + remaining.days;
     hours.textContent = remaining.hours >= 10 ? '' + remaining.hours : '0' + remaining.hours;
@@ -38,7 +56,5 @@ function setTimer(timerNode) {
     seconds.textContent = remaining.seconds >= 10 ? '' + remaining.seconds : '0' + remaining.seconds;
 
     setTimeout(() => setTimer(timerNode), 1e3);
-
 }
 
-setTimer(timer);
