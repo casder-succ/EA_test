6756fcc81bd3c3475852e20f9756ec83915c80aa
casder <casderiopus@gmail.com>
2022-03-14 12:56:52 +0300

    feat: add mail sending

--- js/main.js
+++ js/main.js
@@ -2,33 +2,15 @@ const REGULAR_EMAIL = /^[^@]+@\w+(\.\w+)+\w$/;
 
 document.addEventListener("DOMContentLoaded", () => {
     const timer = document.querySelector('.timer');
-    const sendForm = document.querySelector('.sendmail_form');
+    const sendForm = document.querySelector('#mail_form');
     const popup = document.querySelector('.popup');
 
     popup.querySelector('.popup_back').addEventListener('click', () => closePopup(popup))
-
     popup.querySelectorAll('.popup_close').forEach((btn) => {
         btn.addEventListener('click', () => closePopup(popup));
     })
 
-    sendForm.addEventListener('submit', sendMail);
-
-    async function sendMail(evt) {
-        evt.preventDefault();
-
-        const sendInput = sendForm.querySelector('.form_input');
-
-        if (!REGULAR_EMAIL.test(sendInput.value)) {
-            const params = {
-                title: 'Ahtung!',
-                text: 'Email you entered isn\'t valid',
-            }
-
-            openPopup(popup, params);
-        }
-
-
-    }
+    sendForm.addEventListener('submit', (event) => sendMail(event, sendForm, popup));
 
     setTimer(timer);
 });
@@ -78,9 +60,61 @@ function closePopup(popupNode) {
 }
 
 function openPopup(popupNode, parameters) {
-    popupNode.querySelector('.popup_title').textContent = parameters.title;
-    popupNode.querySelector('.popup_text').textContent = parameters.text;
-
+    popupNode.querySelector('.popup_title').textContent = parameters.title || ' ';
+    popupNode.querySelector('.popup_text').textContent = parameters.text || ' ';
     popupNode.classList.add('active');
 }
 
+function sendMail(evt, sendForm, resultNode) {
+    evt.preventDefault();
+
+    const email = sendForm.email.value;
+
+    if (!REGULAR_EMAIL.test(email)) {
+        const params = {
+            title: 'Oops!',
+            text: 'Email you entered isn\'t valid',
+        }
+
+        openPopup(resultNode, params);
+        return;
+    }
+
+    openPopup(resultNode, {title: 'sending...'});
+
+    fetch("https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "x-rapidapi-host": "rapidprod-sendgrid-v1.p.rapidapi.com",
+            "x-rapidapi-key": "709c9481b3msh63cc2969d037d98p1f5a90jsn255f9e3036f9",
+        },
+        body: {
+            "personalizations": [
+                {
+                    "to": [{"email": email}],
+                    "subject": "Event inviting"
+                }
+            ],
+            "from": {
+                "email": "from_address@example.com"
+            },
+            "content": [
+                {
+                    "type": "text/plain",
+                    "value": "Hi! We wanna invite you to our event: https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                }
+            ]
+        }
+    })
+        .then(() => openPopup(resultNode, {
+                title: 'Success!',
+                text: 'You have successfully subscribed to the email newsletter',
+            })
+        )
+        .catch(() => openPopup(resultNode, {
+                title: 'Error:(',
+                text: 'We can\'t send your data',
+            })
+        )
+}
